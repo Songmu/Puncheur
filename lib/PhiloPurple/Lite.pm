a851b778e5a0cf2e097a5346c0aa59988bd84643
Masayuki Matsuki <y.songmu@gmail.com>
2013-07-18 01:16:47 +0900

    Plugin::HandleStatig

--- lib/PhiloPurple/Lite.pm
+++ lib/PhiloPurple/Lite.pm
@@ -16,56 +16,13 @@ sub import {
         no strict 'refs';
         push @{"$caller\::ISA"}, 'PhiloPurple';
 
+        $caller->load_plugin('HandleStatic');
+        my $to_psgi = $caller->can('to_psgi');
+
+        no warnings 'redefine';
         *{"$caller\::to_psgi"} = sub {
             use strict 'refs';
-            my ($self, %opts) = @_;
-            $self = $self->new unless ref $self;
-
-            my $app = $self->PhiloPurple::to_psgi;
-            if (delete $opts{handle_static} || $self->{handle_static}) {
-                my $vpath = $self->template_dir->[0];
-                require Plack::App::File;
-                my $orig_app = $app;
-                my $app_file_1;
-                my $app_file_2;
-
-                my $base_dir   = $self->base_dir;
-                my $static_dir = File::Spec->catdir( $base_dir, 'static' );
-                $app = sub {
-                    my $env = shift;
-                    my $path_info = $env->{PATH_INFO};
-                    if (my $content = $vpath->{$path_info} and $path_info =~ m{^/}) {
-                        my $ct = Plack::MIME->mime_type($path_info);
-                        state $cache = {};
-                        if ($cache->{$caller}{$path_info}) {
-                            $content = $cache->{$caller}{$path_info};
-                        }
-                        else {
-                            if ($ct !~ /\b(?:text|xml|javascript|json)\b/) {
-                                # binary
-                                $content = MIME::Base64::decode_base64($content);
-                            }
-                            else {
-                                $content = Encode::encode($self->encoding, $content);
-                            }
-                            $cache->{$caller}{$path_info} = $content;
-                        }
-                        return [200, ['Content-Type' => $ct, 'Content-Length' => length($content)], [$content]];
-                    }
-                    elsif ($path_info =~ qr{^(?:/robots\.txt|/favicon\.ico)$}) {
-                        $app_file_1 ||= Plack::App::File->new({ root => $static_dir });
-                        return $app_file_1->call($env);
-                    }
-                    elsif ($path_info =~ m{^/static/}) {
-                        $app_file_2 ||= Plack::App::File->new({ root => $base_dir });
-                        return $app_file_2->call($env);
-                    }
-                    else {
-                        return $orig_app->($env);
-                    }
-                };
-            }
-
+            my $app = $to_psgi->(@_);
             {
                 no strict 'refs';
                 if (my @middlewares = @{"$caller\::_MIDDLEWARES"}) {
@@ -77,7 +34,7 @@ sub import {
                     }
                 }
             }
-            return $app;
+            $app;
         };
 
         *{"$caller\::enable_middleware"} = sub {
---
+++ lib/PhiloPurple/Plugin/HandleStatic.pm
@@ -0,0 +1,63 @@
+package PhiloPurple::Plugin::HandleStatic;
+use 5.010;
+use warnings;
+
+@EXPORT = qw/to_psgi/;
+
+sub to_psgi {
+    my ($self, %opts) = @_;
+    $self = $self->new unless ref $self;
+
+    my $app = $self->PhiloPurple::to_psgi;
+    if (delete $opts{handle_static} || $self->{handle_static}) {
+        my $vpath = sub {
+            for my $dir (@{ $self->template_dir }) {
+                return $dir if ref $dir eq 'HASH';
+            }
+        }->();
+        require Plack::App::File;
+        my $orig_app = $app;
+        my $app_file_1;
+        my $app_file_2;
+
+        my $base_dir   = $self->base_dir;
+        my $static_dir = File::Spec->catdir( $base_dir, 'static' );
+        $app = sub {
+            my $env = shift;
+            my $path_info = $env->{PATH_INFO};
+            if ($vpath and my $content = $vpath->{$path_info} and $path_info =~ m{^/}) {
+                my $ct = Plack::MIME->mime_type($path_info);
+                state $cache = {};
+                if ($cache->{$app_name}{$path_info}) {
+                    $content = $cache->{$app_name}{$path_info};
+                }
+                else {
+                    if ($ct !~ /\b(?:text|xml|javascript|json)\b/) {
+                        # binary
+                        $content = MIME::Base64::decode_base64($content);
+                    }
+                    else {
+                        $content = Encode::encode($self->encoding, $content);
+                    }
+                    $cache->{$app_name}{$path_info} = $content;
+                }
+                return [200, ['Content-Type' => $ct, 'Content-Length' => length($content)], [$content]];
+            }
+            elsif ($path_info =~ qr{^(?:/robots\.txt|/favicon\.ico)$}) {
+                $app_file_1 ||= Plack::App::File->new({ root => $static_dir });
+                return $app_file_1->call($env);
+            }
+            elsif ($path_info =~ m{^/static/}) {
+                $app_file_2 ||= Plack::App::File->new({ root => $base_dir });
+                return $app_file_2->call($env);
+            }
+            else {
+                return $orig_app->($env);
+            }
+        };
+    }
+
+    $app;
+}
+
+1;
